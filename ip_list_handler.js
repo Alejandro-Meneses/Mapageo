8df1353b90404be1933b09dc3c50deb2ecc70e0a
carlosAC03 <cacostac03@educarex.es>
2024-12-17 02:14:40 +0100

    ads

--- ip_list_handler.js
+++ ip_list_handler.js
@@ -1,10 +1,14 @@
 // Lista de IPs que se usarán para mostrar en el mapa
 const ipList = [
-    "8.8.8.8", // Google Public DNS
-    "1.1.1.1", // Cloudflare DNS
-    "208.67.222.222", // OpenDNS
-    "185.228.168.9", // CleanBrowsing
-    "64.6.65.6" // Verisign
+    "8.8.8.8", 
+    "1.1.1.1", 
+    "208.67.222.222", 
+    "185.228.168.9", 
+    "64.6.65.6",
+    "220.40.0.0",
+    "110.20.0.1",
+    "87.37.0.1",
+    "102.177.191.0" 
   ];
   
   // Función para obtener la ubicación de una lista de IPs
